2743f545f43e2ee68f92bd24f57c4abd5d09a9a1
samchris0 <samchris004@gmail.com>
2025-08-08 16:50:28 -0400

    remove clutter

--- clean_up.py
+++
@@ -1,5 +0,0 @@
-from models.historical_rtc_generator import HistoricalRTCGeneratorModel  # import your model class
-from extensions import db
-
-db.session.query(HistoricalRTCGeneratorModel).delete()
-db.session.commit()
